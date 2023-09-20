86aef25a3a83be32fd0f9c2d137f38ff49fe56ca
jim.chen <azx011014@gmail.com>
2023-09-20 19:30:39 +0800

    [feature]: add SongGallery components

---
+++ components/song/gallery/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="mt-5 lg:mt-7.5">
+    <div class="grid grid-cols-2 gap-x-6 gap-y-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+      <div>
+        <SongGalleryPreview />
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -14,41 +14,7 @@ onAuthStateChanged(auth as Auth, (user) => {
       <h1 class="text-2xl font-bold text-white lg:text-3xl xl:text-4xl">
         探索歌曲
       </h1>
-      <div class="mt-5 lg:mt-7.5">
-        <div class="grid grid-cols-2 gap-x-6 gap-y-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-          <div>
-            <div class="group relative">
-              <NuxtLink
-                to="/song/D3yU1dTaU4YQhtNiOpGn"
-                class="block aspect-square overflow-hidden rounded bg-gradient-to-b from-[#383838] to-[#767676]"
-              >
-                <img
-                  src="https://firebasestorage.googleapis.com/v0/b/sound-ebc19.appspot.com/o/covers%2FqEVcwq1gQhgKK21zz2v0rfjKnnp1%2F51421d7d-a2df-44e4-90fa-94ee5dd8c9f0?alt=media&amp;token=28df2fa5-d39d-44fd-bfc2-4cb76f4a6eb9"
-                  alt=""
-                  class="h-full w-full object-cover"
-                >
-              </NuxtLink>
-              <button
-                type="button"
-                class="invisible absolute bottom-5 right-5 z-10 flex h-12 w-12 items-center justify-center rounded-full bg-[#030303] text-white opacity-0 group-hover:visible group-hover:opacity-100"
-              >
-                <IconPlay :size="20" />
-              </button>
-            </div>
-            <h2 class="mt-4 line-clamp-2 font-bold leading-tight text-white">
-              <NuxtLink
-                to="/song/D3yU1dTaU4YQhtNiOpGn"
-                class="hover:underline"
-              >
-                陳約翰 - 當我以為
-              </NuxtLink>
-            </h2>
-            <h3 class="mt-1.5 text-sm text-white/50">
-              秘密 • 古典 • 9 個月前
-            </h3>
-          </div>
-        </div>
-      </div>
+      <SongGallery />
     </div>
   </div>
 </template>
