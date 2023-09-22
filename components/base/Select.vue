4c4660a7cdb23bfce3e2098b371e4815ede742fe
jim.chen <azx011014@gmail.com>
2023-09-22 15:24:58 +0800

    [feature]: add BaseSelect component

---
+++ components/base/Select.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+interface Option {
+  title: string
+  value?: string
+}
+
+const props = defineProps<{
+  name: string
+  label: string,
+  options: Option[]
+}>()
+
+const { value, errorMessage } = useField(() => props.name)
+</script>
+
+<template>
+  <div>
+    <label class="font-bold text-white lg:text-lg">
+      {{ label }}
+    </label>
+    <div class="relative mt-2">
+      <select
+        v-model="value"
+        class="block h-15 w-full appearance-none rounded-full border-[3px] border-transparent bg-[#030303] px-6 text-white outline-none focus:border-[#696969]"
+        :class="{
+          '!border-[#ee2828]': errorMessage
+        }"
+      >
+        <option
+          v-for="option in options"
+          :key="option.title"
+          :value="option.value"
+        >
+          {{ option.title }}
+        </option>
+      </select>
+      <div class="absolute right-6 top-1/2 -translate-y-1/2 text-white">
+        <Icon
+          name="SelectArrow"
+          :size="16"
+        />
+      </div>
+    </div>
+    <span
+      v-if="errorMessage"
+      class="mt-1.5 inline-block text-sm text-[#ee2828]"
+    >
+      {{ errorMessage }}
+    </span>
+  </div>
+</template>
