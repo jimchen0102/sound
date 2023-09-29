2dd28638c992daaf5f51fff7907c08f8d11adafd
jim.chen <azx011014@gmail.com>
2023-09-29 14:06:15 +0800

    [adjust]: adjust user section

--- components/the/Header.vue
+++ components/the/Header.vue
@@ -1,15 +1,21 @@
 <script setup lang="ts">
 import { signOut, Auth } from 'firebase/auth'
+import { vOnClickOutside } from '@vueuse/components'
 
 const auth = useFirebaseAuth()
 const user = useCurrentUser()
 const route = useRoute()
 const { isModalOpen } = useModal('auth')
+const isUserModalOpen = ref(false)
 
 const handleSignOut = async () => {
   await signOut(auth as Auth)
   if (route.name === 'upload' || route.name === 'manage') await navigateTo('/')
 }
+
+watch(() => route.fullPath, () => {
+  isUserModalOpen.value = false
+})
 </script>
 
 <template>
@@ -24,52 +30,14 @@ const handleSignOut = async () => {
         class="w-20 lg:w-auto"
       >
     </NuxtLink>
-    <nav class="pointer-events-auto">
-      <ul
-        v-if="user"
-        class="flex items-center gap-x-2.5 lg:gap-x-5"
-      >
-        <li>
-          <NuxtLink
-            to="/upload"
-            class="group flex h-15 items-center gap-x-2.5 rounded-full bg-[#030303] pl-2.5 pr-6 hover:bg-gradient-to-br hover:from-primary hover:to-secondary"
-          >
-            <div class="flex h-10 w-10 items-center justify-center rounded-full bg-[#212121] text-white">
-              <Icon name="Wave" />
-            </div>
-            <span class="font-bold text-white group-hover:text-black">
-              上傳歌曲
-            </span>
-          </NuxtLink>
-        </li>
-        <li>
-          <NuxtLink
-            to="/manage"
-            class="group flex h-15 items-center gap-x-2.5 rounded-full bg-[#030303] pl-2.5 pr-6 hover:bg-gradient-to-br hover:from-primary hover:to-secondary"
-          >
-            <div class="flex h-10 w-10 items-center justify-center rounded-full bg-[#212121] text-white">
-              <Icon name="Wave" />
-            </div>
-            <span class="font-bold text-white group-hover:text-black">
-              我的歌曲
-            </span>
-          </NuxtLink>
-        </li>
-        <li>
-          <button
-            type="button"
-            class="flex h-15 w-15 items-center justify-center rounded-full bg-[#030303] text-white hover:bg-gradient-to-br hover:from-primary hover:to-secondary hover:text-black"
-            @click="handleSignOut"
-          >
-            <Icon name="Logout" />
-          </button>
-        </li>
-      </ul>
+    <nav
+      v-if="user"
+      class="pointer-events-auto relative"
+    >
       <button
-        v-else
         type="button"
-        class="group flex h-15 items-center gap-x-2.5 rounded-full bg-[#030303] pl-2.5 pr-6 hover:bg-gradient-to-br hover:from-primary hover:to-secondary"
-        @click="isModalOpen = true"
+        class="group flex h-15 items-center gap-x-2.5 rounded-full bg-[#030303] px-2.5 hover:bg-gradient-to-br hover:from-primary hover:to-secondary"
+        @click.stop="isUserModalOpen = !isUserModalOpen"
       >
         <div class="flex h-10 w-10 items-center justify-center rounded-full bg-[#212121] text-white">
           <Icon
@@ -77,11 +45,67 @@ const handleSignOut = async () => {
             :size="20"
           />
         </div>
-        <span class="font-bold text-white group-hover:text-black">
-          登入 / 註冊
-        </span>
       </button>
+      <ul
+        v-if="isUserModalOpen"
+        v-on-click-outside.bubble="() => isUserModalOpen = false"
+        class="absolute right-0 top-[calc(100%+8px)] w-48 rounded-lg bg-black py-4 text-white"
+      >
+        <li>
+          <NuxtLink
+            to="/upload"
+            class="flex items-center gap-x-4 px-6 py-2 text-sm hover:bg-white/10"
+          >
+            <Icon
+              name="Upload"
+              :size="20"
+            />
+            上傳歌曲
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink
+            to="/manage"
+            class="flex items-center gap-x-4 px-6 py-2 text-sm hover:bg-white/10"
+          >
+            <Icon
+              name="Adjust"
+              :size="20"
+            />
+            管理歌曲
+          </NuxtLink>
+        </li>
+        <li>
+          <button
+            type="button"
+            class="flex w-full items-center gap-x-4 px-6 py-2 text-sm hover:bg-white/10"
+            @click="handleSignOut"
+          >
+            <Icon
+              name="Logout"
+              :size="20"
+            />
+            登出
+          </button>
+        </li>
+      </ul>
     </nav>
+    <button
+      v-else
+      type="button"
+      class="group pointer-events-auto flex h-15 items-center gap-x-2.5 rounded-full bg-[#030303] pl-2.5 pr-6 hover:bg-gradient-to-br hover:from-primary hover:to-secondary"
+      @click="isModalOpen = true"
+    >
+      <div class="flex h-10 w-10 items-center justify-center rounded-full bg-[#212121] text-white">
+        <Icon
+          name="User"
+          :size="20"
+        />
+      </div>
+      <span class="font-bold text-white group-hover:text-black">
+        登入 / 註冊
+      </span>
+    </button>
   </header>
 
   <Teleport to="body">
